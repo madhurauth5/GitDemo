a2efd36b8f7b582bad222b24d401f2507b66f97b
madhukanth <madhurauth5@gmail.com>
2021-03-22 13:24:10 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppsAndroid()
+	{
+		System.out.println("NativeAppsAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		
+	}
+	@Test
+	public void elementsUI()
+	{
+		
+	}
+}
